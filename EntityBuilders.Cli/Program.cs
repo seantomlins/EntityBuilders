83fc258d71125558d849bcdecdecad99de7b2cd0
Sean Tomlins <sean@tomlins.nz>
2022-10-13 21:19:13 +1300

    Added auto-generated tag and namespace

--- EntityBuilders.Cli/Program.cs
+++ EntityBuilders.Cli/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using EntityBuilders;
+
 var blogBuilder = new BlogBuilder().Build();
 var postBuilder = new PostBuilder().Build();
