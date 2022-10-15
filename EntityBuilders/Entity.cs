646cca73507daeb91346910622755ccf951bdac7
Sean Tomlins <sean@tomlins.nz>
2022-10-16 02:47:09 +1300

    Refactor to parse the Entity separately from generating the source code

---
+++ EntityBuilders/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EntityBuilders;
+
+internal class Entity
+{
+    public string Name { get; }
+    public string IdPropertyName { get; }
+    public string Namespace { get; }
+    
+    public IEnumerable<PropertyDeclarationSyntax> Properties { get; }
+
+    public Entity(ClassDeclarationSyntax classDeclarationSyntax)
+    {
+        Name = classDeclarationSyntax.Identifier.ToString();
+        Namespace = NamespaceParser.GetNamespace(classDeclarationSyntax);
+        IdPropertyName = IdPropertyParser.GetIdPropertyName(classDeclarationSyntax);
+
+        Properties = classDeclarationSyntax.Members
+            .OfType<PropertyDeclarationSyntax>()
+            .ToImmutableArray();
+    }
+}
