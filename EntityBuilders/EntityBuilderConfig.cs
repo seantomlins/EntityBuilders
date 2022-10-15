2d7437634ef9f294de31966c94d40d78343d6a5f
Sean Tomlins <sean@tomlins.nz>
2022-10-15 22:57:49 +1300

    Refactor config

---
+++ EntityBuilders/EntityBuilderConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace EntityBuilders;
+
+internal class EntityBuilderConfig
+{
+    public string RootNamespace { get; }
+    
+    public EntityBuilderConfig(GeneratorExecutionContext context)
+    {
+        context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("entity_builders.root_namespace",
+            out var rootNamespace);
+        RootNamespace = rootNamespace ?? "EntityBuilders";
+    }
+}
