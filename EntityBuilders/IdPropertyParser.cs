1a048fce159f46684dd2094b1df2f74b552d16e3
Sean Tomlins <sean@tomlins.nz>
2022-10-15 14:36:34 +1300

    Now builds based on IdProperty convention used in Entity

---
+++ EntityBuilders/IdPropertyParser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EntityBuilders;
+
+internal static class IdPropertyParser
+{
+    public static string GetIdPropertyName(ClassDeclarationSyntax classDeclarationSyntax)
+    {
+        var properties = classDeclarationSyntax.Members
+            .OfType<PropertyDeclarationSyntax>();
+
+        return properties.Any(x => x.Identifier.ToString().Equals("Id"))
+            ? "Id"
+            : $"{classDeclarationSyntax.Identifier}Id";
+    }
+}
