5a4ca8e4d8761d6ae9245aec9a00e0d0d6c16b6f
Sean Tomlins <sean@tomlins.nz>
2022-10-17 21:04:02 +1300

    Change example to entity framework collection property

--- ExampleEntities/Blog.cs
+++ ExampleEntities/Blog.cs
@@ -8,5 +8,6 @@ public class Blog
     public int BlogId { get; set; } // Primary Key convention type B
     public string Url { get; set; }
     public int Rating { get; set; }
-    public List<Post> Posts { get; set; }
+    
+    public virtual ICollection<Post> Posts { get; set; }
 }
