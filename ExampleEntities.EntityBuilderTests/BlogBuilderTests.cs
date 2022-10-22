dce3c9b0e239bba94318a8e4328d9baf9bf88f5f
Sean Tomlins <sean@tomlins.nz>
2022-10-22 20:49:03 +1300

    Stubbed out test for AddPost

--- ExampleEntities.EntityBuilderTests/BlogBuilderTests.cs
+++ ExampleEntities.EntityBuilderTests/BlogBuilderTests.cs
@@ -5,15 +5,30 @@ namespace ExampleEntities.EntityBuilderTests;
 
 public class BlogBuilderTests
 {
-   [Fact]
-   public void Constructor_Should_CreateEntityWithId()
-   {
-      // When
-      var builder = new BlogBuilder(new SequentialProvider());
+    [Fact]
+    public void Constructor_Should_CreateEntityWithId()
+    {
+        // When
+        var builder = new BlogBuilder(new SequentialProvider());
 
-      // Then
-      Assert.Equal(1, builder.Entity.BlogId);
-   }
-   
-   // TODO AddPost_Should_AddPostToBlog_And_SetBlogOnPost
+        // Then
+        Assert.Equal(1, builder.Entity.BlogId);
+    }
+
+    [Fact]
+    public void AddPost_Should_AddPostToBlog_And_SetBlogOnPost()
+    {
+        // Given
+        var post = new Post();
+        var builder = new BlogBuilder(new SequentialProvider());
+
+        // When
+        builder.AddPost(post);
+
+        // Then
+        var blog = builder.Entity;
+        Assert.Equal(blog, post.Blog);
+        Assert.Equal(blog.BlogId, post.BlogId);
+        Assert.Contains(post, blog.Posts);
+    }
 }
